d561b3244bb2c1aa2e13c53ccbd91d62875ee1e4
Fedor Indutny <fedor.indutny@gmail.com>
2012-09-03 01:33:14 +0400

    node: improve apis
    * test discovery

--- test/node-test.js
+++ test/node-test.js
@@ -3,16 +3,16 @@ var assert = require('assert'),
     Buffer = require('buffer').Buffer;
 
 describe('DHT.js/Bencode', function() {
-  var a, b;
+  var nodes;
 
   beforeEach(function(callback) {
-    var waiting = 2;
+    var waiting = 20;
 
-    a = dht.node.create();
-    b = dht.node.create();
-
-    a.once('listening', finish);
-    b.once('listening', finish);
+    nodes = [];
+    for (var i = 0; i < waiting; i++) {
+      nodes[i] = dht.node.create();
+      nodes[i].once('listening', finish);
+    }
 
     function finish() {
       if (--waiting !== 0) return;
@@ -22,14 +22,34 @@ describe('DHT.js/Bencode', function() {
   });
 
   afterEach(function() {
-    a.close();
-    b.close();
+    for (var i = 0; i < nodes.length; i++) {
+      nodes[i].close();
+    }
   });
 
   it('should successfully send/receive pings', function(callback) {
-    a.sendPing(b, function(err) {
+    nodes[0].sendPing(nodes[1], function(err) {
       assert(err === null);
       callback();
     });
+  });
+
+  it('should successfully discover peers', function(callback) {
+    var infohash = new Buffer([0,5,4,3,2,1,6,7,8,9,
+                               0,1,3,5,8,9,6,7,8,9]);
+
+    nodes[0].addNode(nodes[1]);
+    nodes[1].addNode(nodes[2]);
+    nodes[2].addNode(nodes[3]);
+
+    nodes[0].addPeer(infohash, '127.0.0.1', 13589);
+    nodes[2].on('peer:new', function(ih, peer) {
+      if (infohash.toString('hex') !== ih.toString('hex')) return;
+      assert.equal(peer.address, '127.0.0.1');
+      assert.equal(peer.port, nodes[0].port);
+      callback();
+    });
+
+    nodes[0].announce();
   });
 });
