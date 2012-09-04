9eb278da1275a8a63cb72c0ebc9f7dfe8b4d0897
Fedor Indutny <fedor.indutny@gmail.com>
2012-09-04 12:07:35 +0400

    bucket: use big numbers

---
+++ test/bn-test.js
@@ -0,0 +1,76 @@
+var assert = require('assert'),
+    dht = require('..'),
+    bn = dht.bn,
+    Buffer = require('buffer').Buffer;
+
+describe('DHT.js/BigNumbers', function() {
+  it('should add #1', function() {
+    var a = new Buffer('0001ffffffff', 'hex'),
+        b = new Buffer('0001', 'hex');
+
+    assert.equal(bn.add(a, b).toString('hex'), '000200000000');
+  });
+
+  it('should add #2', function() {
+    var a = new Buffer('000123456789', 'hex'),
+        b = new Buffer('000123456789', 'hex');
+
+    assert.equal(bn.add(a, b).toString('hex'), '0002468acf12');
+  });
+
+  it('should sub #1', function() {
+    var a = new Buffer('000123456789', 'hex'),
+        b = new Buffer('000123456789', 'hex');
+
+    assert.equal(bn.sub(a, b).toString('hex'), '000000000000');
+  });
+
+  it('should sub #2', function() {
+    var a = new Buffer('0005124ab125', 'hex'),
+        b = new Buffer('000123456789', 'hex');
+
+    assert.equal(bn.sub(a, b).toString('hex'), '0003ef05499c');
+  });
+
+  it('should shr #1', function() {
+    var a = new Buffer('000100000000', 'hex');
+
+    assert.equal(bn.shr(a).toString('hex'), '000080000000');
+  });
+
+  it('should shr #2', function() {
+    var a = new Buffer('12345678', 'hex');
+
+    assert.equal(bn.shr(a).toString('hex'), '091a2b3c');
+  });
+
+  it('should compare #1', function() {
+    var a = new Buffer('12345678', 'hex'),
+        b = new Buffer('12345678', 'hex');
+
+    assert.equal(bn.compare(a, b), 0);
+  });
+
+  it('should compare #2', function() {
+    var a = new Buffer('12345678', 'hex'),
+        b = new Buffer('12355678', 'hex');
+
+    assert.equal(bn.compare(a, b), -1);
+  });
+
+  it('should compare #3', function() {
+    var a = new Buffer('13345678', 'hex'),
+        b = new Buffer('12359999', 'hex');
+
+    assert.equal(bn.compare(a, b), 1);
+  });
+
+  it('should random', function() {
+    var a = new Buffer('13345678', 'hex'),
+        b = new Buffer('15359999', 'hex'),
+        r = bn.random(a, b);
+
+    assert(bn.compare(a, r) <= 0);
+    assert(bn.compare(r, b) <= 0);
+  });
+});
